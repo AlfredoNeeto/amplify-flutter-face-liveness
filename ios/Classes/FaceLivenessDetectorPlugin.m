ae51a4393d80978169bd72a3674a1e5b24f9dc43
Alfredo Neto <alfredo.neto@agendaassessoria.com.br>
2025-07-16 08:35:36 -0400

    Ajustes adicionais

--- ios/Classes/FaceLivenessDetectorPlugin.m
+++ ios/Classes/FaceLivenessDetectorPlugin.m
@@ -1,14 +1,12 @@
 #import "FaceLivenessDetectorPlugin.h"
 #if __has_include(<face_liveness_detector/face_liveness_detector-Swift.h>)
 #import <face_liveness_detector/face_liveness_detector-Swift.h>
-#elif __has_include(<face-liveness-detector/face-liveness-detector-Swift.h>)
-#import <face-liveness-detector/face-liveness-detector-Swift.h>
 #else
 #import "face_liveness_detector-Swift.h"
 #endif
 
 @implementation FaceLivenessDetectorPlugin
 + (void)registerWithRegistrar:(NSObject<FlutterPluginRegistrar>*)registrar {
-  [FaceLivenessDetectorPlugin registerWithRegistrar:registrar];
+  [SwiftFaceLivenessDetectorPlugin registerWithRegistrar:registrar];
 }
 @end 
--- ios/face_liveness_detector/Sources/face_liveness_detector/FaceLivenessPlugin.swift
+++ ios/face_liveness_detector/Sources/face_liveness_detector/FaceLivenessPlugin.swift
@@ -3,20 +3,28 @@ import UIKit
 
 public class FaceLivenessPlugin: NSObject, FlutterPlugin {
     public static func register(with registrar: FlutterPluginRegistrar) {
-        let handler = EventStreamHadler()
-        let eventChannel = FlutterEventChannel(name: "face_liveness_event", binaryMessenger: registrar.messenger())
-        eventChannel.setStreamHandler(handler)
-        
-        let instance = FaceLivenessPlugin()
-        let factory = FaceLivenessViewFactory(messenger: registrar.messenger(), handler: handler)
-        registrar.register(factory, withId: "face_liveness_view")
-        
-        print("FaceLivenessPlugin initialized with custom credentials provider")
-    }
+    let channel = FlutterMethodChannel(name: "face_liveness_detector", binaryMessenger: registrar.messenger())
+    let instance = FaceLivenessDetectorPlugin()
+    registrar.addMethodCallDelegate(instance, channel: channel)
 
-    public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result(FlutterMethodNotImplemented)
+    let handler = EventStreamHadler()
+    let eventChannel = FlutterEventChannel(name: "face_liveness_event", binaryMessenger: registrar.messenger())
+    eventChannel.setStreamHandler(handler)
+    
+    let factory = FaceLivenessViewFactory(messenger: registrar.messenger(), handler: handler)
+    registrar.register(factory, withId: "face_liveness_view")
+    
+    print("FaceLivenessPlugin initialized with custom credentials provider")
+  }
+  
+  public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
+
+    if call.method == "getPlatformVersion" {
+      result("iOS " + UIDevice.current.systemVersion)
+    } else {
+      result(FlutterMethodNotImplemented)
     }
+  }
 }
 
 class EventStreamHadler: NSObject, FlutterStreamHandler {
